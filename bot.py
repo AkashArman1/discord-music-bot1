30c0b36789afe89d82c7922795a998fa0ffde16f
AkashArman1 <akasharman5678@gmail.com>
2025-10-16 23:46:43 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+import wavelink
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Connect to Lavalink node
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+    if not hasattr(bot, 'wavelink'):
+        bot.wavelink = wavelink.Client(bot=bot)
+        await bot.wavelink.initiate_node(
+            host='lava.link',      # Free public Lavalink
+            port=2333,
+            password='youshallnotpass'
+        )
+    print('Bot ready and connected to Lavalink!')
+
+# Join VC
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect(cls=wavelink.Player)
+        await ctx.send(f"Joined {channel}")
+    else:
+        await ctx.send("You are not in a voice channel!")
+
+# Leave VC
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnected")
+    else:
+        await ctx.send("I am not in a voice channel!")
+
+# Play music
+@bot.command()
+async def play(ctx, *, query: str):
+    if not ctx.voice_client:
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        else:
+            return await ctx.send("Join a voice channel first!")
+
+    tracks = await wavelink.YouTubeTrack.search(query=query, return_first=True)
+    await ctx.voice_client.play(tracks)
+    await ctx.send(f"Now playing: {tracks.title}")
+
+# Skip
+@bot.command()
+async def skip(ctx):
+    if ctx.voice_client.is_playing():
+        await ctx.voice_client.stop()
+        await ctx.send("Skipped!")
+    else:
+        await ctx.send("Nothing is playing!")
+
+import os
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
